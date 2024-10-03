e96a0113fecd4e4c3ff2fa57dd23a31d370bbe74
Jiaying Su <jsu5@inside.artcenter.edu>
2024-10-04 03:03:40 +0800

    homework

--- week3/script.js
+++ week3/script.js
@@ -1,9 +1,11 @@
-let thumbnails = document.querySelectorAll(".thumbnail");
-let largeImage = document.getElementById("largeImage");
 
-thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener("click", function() {
-        // 将大图的 src 改为点击缩略图对应的图片
-        largeImage.src = `images/panda${index + 1}.jpg`;
+let thumbnails = document.querySelectorAll('.thumbnail');
+let largeImage = document.getElementById('largeImage');
+
+
+thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', function() {
+        
+        largeImage.src = thumbnail.src;
     });
 });
