35c4171e31bc5c55039bfc728e4158695e685a40
Jiaying Su <jsu5@inside.artcenter.edu>
2024-10-04 02:07:45 +0800

    homework

--- week3/script.js
+++ week3/script.js
@@ -1,39 +1,9 @@
-let DOMsection =document.getElementById("DOMoptions")
-let newParaph =document.createElement("p")
-let buttonColor =document.getElementById("btnColorChange")
-let toggleImage =document.getElementById("btnImageToggle")
-let galleryIMG =document.getElementById("imageGallery").children[0]
+let thumbnails = document.querySelectorAll(".thumbnail");
+let largeImage = document.getElementById("largeImage");
 
-console.log(document.getElementById("imageGallery").children[0])
-
-document.getElementById("DOMoptions").style.backgroundColor="rgb(215,220,250)"
-
-newParaph.innerText ="Hello World"
-DOMsection.appendChild(newParaph)
-
-let imageToggle= function(){
-    console.log("fire")
-    console.log(galleryIMG.src)
-    if(galleryIMG.src.includes("panda1")){
-        console.log("panda1")
-        galleryIMG.src ="images/panda2.jpg"
-
-    }
-    else{
-        console.log("panda2")
-        galleryIMG.src ="images/panda1.jpg"
-    }
-
-}
-
-toggleImage.addEventListener("click", imageToggle)
-buttonColor.addEventListener("click", function(){
-    let redPortion=Math.random()*100+155
-    let greenPortion=Math.random()*255
-    let bluePortion=Math.random()*200+55
-
-    let randomColor="rgb("+redPortion+","+greenPortion+","+bluePortion+")"
-    console.log(randomColor)
-
-    DOMsection.style.backgroundColor=randomColor
-})
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", function() {
+        // 将大图的 src 改为点击缩略图对应的图片
+        largeImage.src = `images/panda${index + 1}.jpg`;
+    });
+});
